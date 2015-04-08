695d7450d76616598b32a7dd8b615e9a64354f99
harik <hamouda.form@gmail.com>
2015-04-08 10:04:22 +0000

    creation d'une application vide

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Monblog::Application.config.secret_key_base = 'd13a3503f5916c5ecd805f71d4e8737284f3d745a7589f48264034767d7888b5fe506ae9b886e8f0f3f91cfb08d96a137363ebdf34ebdcdeed27636711ab63f6'
